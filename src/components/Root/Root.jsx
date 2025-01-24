9b8ef90cb7810cc6c760e3b074b4bcfed8587b83
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-01-24 09:16:02 +0600

    Project Setup

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Root from './components/Root/Root'
+import Home from './components/Home/Home'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
