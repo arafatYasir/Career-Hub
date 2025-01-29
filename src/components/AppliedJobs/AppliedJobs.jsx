2fc4ce2a4a41fb68d76c4c34ccc27990203a7bc1
arafatYasir <98328335+arafatYasir@users.noreply.github.com>
2025-01-29 19:12:28 +0600

    Bug Fixed

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -7,13 +7,17 @@ const AppliedJobs = () => {
         <div>
             <h2 className="mt-11 text-4xl md:text-5xl font-bold text-[#1A1919] text-center">Track Your Applications</h2>
             <p className="text-[15px] md:text-lg text-[#757575] leading-7 text-center mt-4 font-medium max-w-[800px] mx-auto">Effortlessly manage your job applications in one place, with key details like titles, companies, and locations displayed for easy access. Stay organized and focused on your career goals!</p>
-            <h4 className="text-center text-lg font-semibold mt-6">Total Applied: {appliedJobs.length}</h4>
-            
-            <div className="flex flex-wrap justify-between gap-6 mt-11">
-                {
-                    appliedJobs.map(job => <AppliedJob key={job.job_id} job={job} />)
-                }
-            </div>
+            <h4 className="text-center text-lg font-semibold mt-6">Total Applied: {appliedJobs ? appliedJobs.length : 0}</h4>
+
+            {
+                appliedJobs && (
+                    <div className="flex flex-wrap justify-between gap-6 mt-11">
+                        {
+                            appliedJobs.map(job => <AppliedJob key={job.job_id} job={job} />)
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 };
